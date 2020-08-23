59427d98685bddd5a6d9fad60fed88eadaee574f
csalinasonline <csalinasonline@gmail.com>
2020-08-22 20:50:23 -0700

    Enable datalogging at approx 50Hz.  Read timestamp, accel and gyro.

--- ProjectStrydCopy.ino
+++ ProjectStrydCopy.ino
@@ -1,4 +1,6 @@
 
+#include <Arduino_LSM9DS1.h>
+ 
 int led = 13;
 
 // the setup routine runs once when you press reset:
@@ -7,14 +9,59 @@ void setup() {
   pinMode(led, OUTPUT);
 
   Serial1.begin(115200);
+  Serial1.println("[START]");
 
-  Serial1.println("Example print to OpenLog");
+  // Check IMU
+  if (!IMU.begin()) {
+    Serial1.println("[INIT CHECK], IMU, IMU not initialized.");
+    while (1);
+  }
+  Serial1.println("[INIT CHECK], IMU, IMU initialized.");
+  
+  Serial1.println("[VAL CHECK], IMU, ACCL sample rate: ");
+  Serial.print(IMU.accelerationSampleRate());
+  Serial.println(" Hz");
+  Serial1.println("[VAL CHECK], IMU, ACCL units: G's");   
+  Serial1.println("[VAL CHECK], IMU, GYRO sample rate: ");
+  Serial.print(IMU.gyroscopeSampleRate());
+  Serial.println(" Hz");
+  Serial1.println("[VAL CHECK], IMU, GYRO units: deg/sec");  
+  Serial1.println("[HEADER INFO]");  
+  Serial1.println("time, accl_x, accl_y, accl_z, gyro_x, gyro_y, gyro_z");  
 }
 
 // the loop routine runs over and over again forever:
 void loop() {
+  float accl_x, accl_y, accl_z;
+  float gyro_x, gyro_y, gyro_z;
+  
   digitalWrite(led, HIGH);   // turn the LED on (HIGH is the voltage level)
-  delay(1000);               // wait for a second
-  digitalWrite(led, LOW);    // turn the LED off by making the voltage LOW
-  delay(1000);               // wait for a second
+
+  Serial1.print(millis());
+  Serial1.print(", ");
+  
+  if (IMU.accelerationAvailable()) {
+    IMU.readAcceleration(accl_x, accl_y, accl_z);
+    Serial1.print(accl_x);
+    Serial1.print(", ");
+    Serial1.print(accl_y);
+    Serial1.print(", ");
+    Serial1.print(accl_z);
+  }
+
+  Serial1.print(", ");
+  
+  if (IMU.gyroscopeAvailable()) {
+    IMU.readGyroscope(gyro_x, gyro_y, gyro_z);
+    Serial1.print(gyro_x);
+    Serial1.print(", ");
+    Serial1.print(gyro_y);
+    Serial1.print(", ");
+    Serial1.println(gyro_z);
+  }
+
+  digitalWrite(led, LOW);  // turn the LED off by making the voltage LOW
+  delay(10);               // wait for a bit
+  
+  
 }
