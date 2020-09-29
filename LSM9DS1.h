4368dc036e3d4f71a29e68222fb6c8f0ec278f7d
csalinasonline <csalinasonline@gmail.com>
2020-09-28 18:39:28 -0700

    Added IMU source directly into project with some mofications from original REPO.  https://github.com/arduino-libraries/Arduino_LSM9DS1

---
+++ LSM9DS1.h
@@ -0,0 +1,81 @@
+/*
+  This file is part of the Arduino_LSM9DS1 library.
+  Copyright (c) 2019 Arduino SA. All rights reserved.
+
+  This library is free software; you can redistribute it and/or
+  modify it under the terms of the GNU Lesser General Public
+  License as published by the Free Software Foundation; either
+  version 2.1 of the License, or (at your option) any later version.
+
+  This library is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+  Lesser General Public License for more details.
+
+  You should have received a copy of the GNU Lesser General Public
+  License along with this library; if not, write to the Free Software
+  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+*/
+
+#include <Arduino.h>
+#include <Wire.h>
+
+#define LSM9DS1_ADDRESS            0x6b
+
+#define LSM9DS1_WHO_AM_I           0x0f
+#define LSM9DS1_CTRL_REG1_G        0x10
+#define LSM9DS1_STATUS_REG         0x17
+#define LSM9DS1_OUT_X_G            0x18
+#define LSM9DS1_CTRL_REG6_XL       0x20
+#define LSM9DS1_CTRL_REG8          0x22
+#define LSM9DS1_OUT_X_XL           0x28
+
+// magnetometer
+#define LSM9DS1_ADDRESS_M          0x1e
+
+#define LSM9DS1_CTRL_REG1_M        0x20
+#define LSM9DS1_CTRL_REG2_M        0x21
+#define LSM9DS1_CTRL_REG3_M        0x22
+#define LSM9DS1_STATUS_REG_M       0x27
+#define LSM9DS1_OUT_X_L_M          0x28
+
+class LSM9DS1Class {
+  public:
+    LSM9DS1Class(TwoWire& wire);
+    virtual ~LSM9DS1Class();
+
+    int begin();
+    void end();
+
+    // Controls whether a FIFO is continuously filled, or a single reading is stored.
+    // Defaults to one-shot.
+    void setContinuousMode();
+    void setOneShotMode();
+
+    // Accelerometer
+    virtual int readAcceleration(float& x, float& y, float& z); // Results are in G (earth gravity).
+    virtual int accelerationAvailable(); // Number of samples in the FIFO.
+    virtual float accelerationSampleRate(); // Sampling rate of the sensor.
+
+    // Gyroscope
+    virtual int readGyroscope(float& x, float& y, float& z); // Results are in degrees/second.
+    virtual int gyroscopeAvailable(); // Number of samples in the FIFO.
+    virtual float gyroscopeSampleRate(); // Sampling rate of the sensor.
+
+    // Magnetometer
+    virtual int readMagneticField(float& x, float& y, float& z); // Results are in uT (micro Tesla).
+    virtual int magneticFieldAvailable(); // Number of samples in the FIFO.
+    virtual float magneticFieldSampleRate(); // Sampling rate of the sensor.
+
+    int readRegister(uint8_t slaveAddress, uint8_t address);
+    int readRegisters(uint8_t slaveAddress, uint8_t address, uint8_t* data, size_t length);
+    int writeRegister(uint8_t slaveAddress, uint8_t address, uint8_t value);
+
+  private:
+    bool continuousMode;
+
+  private:
+    TwoWire* _wire;
+};
+
+extern LSM9DS1Class IMU;
